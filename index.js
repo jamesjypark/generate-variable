a016c8d91a3282a68ac51426bad96c5800dd4d61
Juyoung Park <james.park.victoria@gmail.com>
2019-08-23 12:53:52 -0700

    Implemented variable to number reverse function

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ const numToAlpha = num => {
   return String.fromCharCode(num + startCharCode);
 };
 
+const alphaToNum = char => {
+  let num = char.charCodeAt(0);
+  if (num < 91) {
+    return num - 65;
+  }
+  return num - 71;
+};
+
 const numToVariable = num => {
   if (num <= 51) {
     return numToAlpha(num);
@@ -29,4 +37,14 @@ const numToVariable = num => {
   }
 };
 
+const variableToNum = str => {
+  let sum = 0;
+  while (str) {
+    sum += alphaToNum(str[0]) * Math.pow(52, str.length - 1);
+    str = str.slice(1);
+  }
+  return sum;
+};
+
 module.exports = numToVariable;
+module.exports = variableToNum;
